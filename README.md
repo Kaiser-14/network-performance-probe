211b4bd1de09a02e7279dff1960cf8e82c982041
Alberto del Río Ponce <alberdrp@gmail.com>
2023-09-28 16:28:21 +0200

    Include documentation and essential metrics

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# network-performance-probe
+# Network Performance Probe
+
+![Python](https://img.shields.io/badge/Python-3.x-blue.svg)
+
+A Python-based network performance probe that measures various metrics using socket communication.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Options](#options)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+This network performance probe is designed to measure various network metrics using socket communication. It allows you to gather critical information about network performance, such as bandwidth, throughput, latency, packet loss rate, jitter, retransmission rate, and network interface statistics.
+
+## Features
+
+- Measures multiple network performance metrics.
+- Easy-to-use command-line interface.
+- Customizable test duration, target host, and port.
+- Detailed verbose output option.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Configure a web server like Apache or Nginx, as port 80 is the default port for HTTP traffic. For example, on Ubuntu, you can follow these steps:
+  - Install Apache2 using the `apt` package manager:
+
+    ```bash
+    sudo apt update
+    sudo apt install apache2
+    ```
+
+  - Start and enable Apache:
+
+    ```bash
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+    ```
+
+  - Check the firewall to allow incoming HTTP traffic:
+
+    ```bash
+    sudo ufw allow 80/tcp
+    ```
+
+  - Verify that Apache is properly listening on port 80:
+
+    ```bash
+    sudo ss -tuln | grep :80
+    ```
+
+### Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/network-performance-probe.git
+   ```
+   
+2. Navigate to the project directory:
+
+   ```bash
+   cd network-performance-probe
+   ```
+
+3. Install the required dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+To measure network performance, run the following command:
+
+   ```bash
+   python network_probe.py --host <target_host> [options]
+   ```
+
+Replace <target_host> with the IP address or hostname of the target you want to measure.
+
+## Options
+
+- '--verbose': Print verbose output.
+- '--host <target_host>': Specify the target host IP address (required).
+- '--port <target_port>': Specify the target port (default: 80).
+- '--duration <duration>': Test duration in seconds (default: 4).
+- '--bandwidth': Enable bandwidth measurement.
+- '--throughput': Enable throughput measurement.
+- '--congestion': Enable network congestion measurement.
+- '--packet-loss': Enable packet loss rate measurement.
+- '--latency': Enable latency measurement.
+- '--jitter': Enable jitter measurement.
+- '--retransmission-rate': Enable retransmission rate measurement.
+- '--interface-stats': Enable network interface statistics measurement.
+
+Example usage:
+
+   ```bash
+   python network_probe.py --host 192.168.1.291 --port 80 --bandwidth --latency --congestion --verbose
+   ```
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ socket_measurement.py
@@ -0,0 +1,504 @@
+import argparse
+import json
+import random
+import signal
+import socket
+import struct
+import time
+from datetime import datetime
+
+import numpy as np
+import psutil
+from ping3 import ping
+
+
+def send_icmp_echo_request(sock, dst_ip, icmp_id, icmp_seq):
+	# ICMP Echo Request Type and Code
+	icmp_type = 8
+	icmp_code = 0
+
+	# ICMP Header Checksum (0 for now, will be calculated later)
+	icmp_checksum = 0
+
+	# Generate the ICMP payload (some data for verification)
+	_data = b'abcdefghijklmnopqrstuvwxyz' * 4
+
+	# Calculate ICMP header checksum
+	# icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq)
+	# icmp_checksum = calculate_checksum(icmp_header + data)
+	# icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq)
+
+	# Generate the ICMP header without the checksum
+	icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, 0, icmp_id, icmp_seq)
+	# Calculate ICMP header checksum
+	icmp_checksum = calculate_checksum(icmp_header + _data)
+	# Update the ICMP header with the calculated checksum
+	icmp_header = struct.pack('!BBHHH', icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq)
+
+	# Send the ICMP Echo Request packet
+	sock.sendto(icmp_header + _data, (dst_ip, 0))
+
+
+def receive_icmp_echo_reply(sock, icmp_id, icmp_seq, timeout):
+	start_time = time.time()
+	while True:
+		# Set socket timeout to avoid indefinite blocking
+		_elapsed_time = time.time() - start_time
+		remaining_time = timeout - _elapsed_time
+		if remaining_time <= 0:
+			break
+		sock.settimeout(remaining_time)
+
+		try:
+			data, address = sock.recvfrom(1024)
+
+			# Unpack the ICMP header
+			icmp_header = data[20:28]
+			icmp_type, icmp_code, _, received_id, received_seq = struct.unpack('!BBHHH', icmp_header)
+
+			# Check if the received packet is an ICMP Echo Reply with the correct ID and Sequence
+			if icmp_type == 0 and icmp_code == 0 and received_id == icmp_id and received_seq == icmp_seq:
+				print(received_seq)
+				rtt = time.time() - start_time
+				return True, rtt
+
+		except socket.timeout:
+			break
+
+	return False, None
+
+
+def calculate_checksum(_data):
+	# Calculate the ICMP header checksum
+	checksum = 0
+	for i in range(0, len(_data), 2):
+		word = (_data[i] << 8) + _data[i + 1]
+		checksum += word
+		checksum = (checksum & 0xFFFF) + (checksum >> 16)
+	checksum = ~checksum & 0xFFFF
+	return checksum
+
+
+def measure_packet_loss2(host, count=10, timeout=1):
+	try:
+		# Create a raw socket with ICMP protocol
+		icmp_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+
+		# Random ICMP ID and Sequence numbers for identification
+		icmp_id = random.randint(0, 65535)
+		packet_loss = 0
+
+		for seq in range(count):
+			send_icmp_echo_request(icmp_sock, host, icmp_id, seq)
+			# time.sleep(1)
+			if not receive_icmp_echo_reply(icmp_sock, icmp_id, seq, timeout):
+				packet_loss += 1
+
+		icmp_sock.close()
+
+		packet_loss_rate = (packet_loss / count) * 100
+		return packet_loss_rate
+
+	except (socket.gaierror, PermissionError):
+		return None  # Host is unreachable
+
+
+def measure_bandwidth():
+	# Extract network statistics from all the interfaces
+	_network_interface_stats = psutil.net_if_stats()
+
+	_net_dict = {}
+
+	# Exclude not important interfaces
+	# FIXME: Check with Kubernetes system
+	excluded_interfaces = ['Loopback Pseudo-Interface 1', 'vEthernet (WSL)']
+
+	# Get the maximum available bandwidth
+	for _interface, _data in _network_interface_stats.items():
+		if _data.isup and _interface not in excluded_interfaces:
+			# _net_dict[_interface] = {'value': _data.speed, 'metric': 'Mbps'}
+			_net_dict[_interface] = _data.speed
+
+	return _net_dict
+
+
+def measure_throughput(host, port, duration, data_size_kb=1024):
+	_data = b'X' * (data_size_kb * 1024)
+	_measure = 'Mbps'
+
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((host, port))
+
+		start_time = time.time()
+		total_bytes_sent = 0
+
+		while time.time() - start_time < duration:
+			bytes_sent = s.send(_data)
+			# bytes_sent = s.recv(1024)
+			total_bytes_sent += bytes_sent
+
+		_elapsed_time = time.time() - start_time
+		if _measure == 'KB/s':
+			throughput_kbps = (total_bytes_sent / 1024) / _elapsed_time  # KB/s
+		else:
+			throughput_mbps = (total_bytes_sent * 8e-6) / _elapsed_time  # Mbps
+
+	return throughput_mbps, _measure
+
+
+def measure_packet_loss(host, count=40, timeout=0.1):
+
+	received_packets = 0
+
+	for _ in range(count):
+		response_time = ping(host, timeout)
+		if response_time is not None:
+			received_packets += 1
+
+		time.sleep(0.05)  # Adjust the interval between pings as needed # FIXME: Consider latency
+
+	packet_loss_rate = ((count - received_packets) / count) * 100
+
+	return packet_loss_rate
+
+
+def measure_latency(host, port):
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.settimeout(5)  # Set a timeout for the connection attempt
+		start_time = time.time()
+
+		try:
+			s.connect((host, port))
+			_elapsed_time = time.time() - start_time
+			latency_ms = _elapsed_time * 1000
+
+		except socket.timeout:
+			latency_ms = None  # Timeout occurred, host is unreachable
+
+	return latency_ms
+
+
+def measure_jitter(destination, count):
+
+	rtt_packages = []
+
+	for _ in range(count):
+		response_time = ping(destination) * 1000
+		if response_time is not None:
+			rtt_packages.append(response_time)
+
+		time.sleep(0.1)  # Adjust the interval between pings as needed
+
+	time_diffs = [rtt_packages[i] - rtt_packages[i - 1] for i in range(1, len(rtt_packages))]
+
+	jitter = round(np.std(time_diffs), 2)
+
+	return jitter
+
+
+def measure_retransmission_rate(host, port, data_size_kb=1024, duration_sec=2):
+	_data = b'X' * (data_size_kb * 1024)
+
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((host, port))
+
+		start_time = time.time()
+		total_packets_sent = 0
+		retransmitted_packets = 0
+
+		while time.time() - start_time < duration_sec:
+			try:
+				s.send(_data)
+				total_packets_sent += 1
+			except socket.error:
+				retransmitted_packets += 1
+
+		retransmission_rate = (retransmitted_packets / total_packets_sent) * 100
+		return retransmission_rate
+
+
+def calculate_congestion(duration):
+	# Get the maximum available bandwidth for the network interface
+	_network_interface_bw = measure_bandwidth()
+
+	network_info = psutil.net_io_counters(pernic=True)
+	network_info = {key: network_info[key]	for key in _network_interface_bw.keys()}
+
+	# Sleep time to retrieve data
+	time.sleep(duration)
+
+	network_info_new = psutil.net_io_counters(pernic=True)
+	network_info_new = {key: network_info_new[key] for key in _network_interface_bw.keys()}
+
+	utilization = {}
+
+	for interface in _network_interface_bw.keys():
+		old_data = network_info[interface]
+		new_data = network_info_new[interface]
+
+		if not new_data:
+			continue
+
+		total_bytes = new_data.bytes_sent - old_data.bytes_sent + new_data.bytes_recv - old_data.bytes_recv
+
+		utilization[interface] = round(
+			(total_bytes * 1e-6 * 8 / duration / _network_interface_bw[interface]) * 100, 2)
+
+	return utilization
+
+
+def get_network_interface_statistics(interface_name):
+	interfaces = psutil.net_if_stats()
+
+	if interface_name in interfaces:
+		interface_stats = interfaces[interface_name]
+		return interface_stats
+	else:
+		return None
+
+
+# Main function to perform on-demand measurements
+def perform_measurements(
+		verbose, host, port, duration, measure_bandwidth_flag, measure_throughput_flag=True,
+		measure_packet_loss_flag=True, measure_latency_flag=True, measure_jitter_flag=True,
+		measure_retransmission_rate_flag=True, measure_congestion_flag=True, measure_interface_stats_flag=True):
+	start_time = time.time()
+
+	# Initialize variables
+	_measurements = {
+		'bandwidth': {
+		},
+		'throughput': {
+		},
+		'congestion': {
+		},
+		'packet_loss': {
+		},
+		'latency': {
+		},
+		'jitter': {
+		},
+		'retransmission_rate': {
+		},
+		'interface_stats': {
+		},
+	}
+
+	print('---------------------------')
+	print('Performing network measurements...')
+	print(f'Target host: {host}')
+	print(f'Target port: {port}')
+	print('---------------------------')
+
+	if measure_bandwidth_flag:
+		# Get the available bandwidth for all the network interfaces
+		start_bw_time = time.time()
+		_network_interface_stats = measure_bandwidth()
+		_measurements['bandwidth']['value'] = _network_interface_stats
+		_measurements['bandwidth']['metric'] = 'Mbps'
+		bw_elapsed_time = time.time() - start_bw_time
+
+		interface = 'Wi-Fi'
+		measure = 'Mbps'
+		bandwidth = _network_interface_stats[interface]
+
+		if verbose:
+			print(f'Measured Bandwidth on interface {interface}: {bandwidth:.2f} {measure}')
+			print(f'Time taken for Bandwidth measurement: {bw_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_throughput_flag:
+		start_br_time = time.time()
+		throughput, measure = measure_throughput(host, port, duration/2)
+		_measurements['throughput']['value'] = float(f'{throughput:.2f}')
+		_measurements['throughput']['metric'] = measure
+		br_elapsed_time = time.time() - start_br_time
+		if verbose:
+			print(f'Measured Throughput: {throughput:.2f} {measure}')
+			print(f'Time taken for Throughput measurement: {br_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_packet_loss_flag:
+		start_loss_time = time.time()
+		packet_loss = measure_packet_loss(host)
+		_measurements['packet_loss']['value'] = packet_loss
+		_measurements['packet_loss']['metric'] = '%'
+		loss_elapsed_time = time.time() - start_loss_time
+		if verbose:
+			if packet_loss is not None:
+				print(f'Measured Packet Loss Rate: {packet_loss:.2f}%')
+			else:
+				print('Packet loss measurement failed. Host may be unreachable.')
+			print(f'Time taken for Packet Loss measurement: {loss_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_latency_flag:
+		start_latency_time = time.time()
+		# latency, jitter = measure_latency(host)
+		latency = measure_latency(host, port)
+		# latency, rtt, jitter = measure_latency_rtt_jitter(host, port)
+		_measurements['latency']['value'] = float(f'{latency:.2f}')
+		_measurements['latency']['metric'] = 'ms'
+		latency_elapsed_time = time.time() - start_latency_time
+		if verbose:
+			if latency is not None:
+				print(f'Measured Latency: {latency:.2f} ms')
+				# print(f'Measured Jitter: {jitter:.2f} ms')
+			else:
+				print('Latency measurement failed. Host may be unreachable.')
+			print(f'Time taken for Latency measurement: {latency_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_jitter_flag:
+		start_jitter_time = time.time()
+		# jitter, _ = measure_latency(host, count=1)
+		# results = measure_latency_and_jitter(host, port)
+		jitter = measure_jitter(host, count=10)
+		_measurements['jitter']['value'] = jitter
+		_measurements['jitter']['metric'] = 'ms'
+		jitter_elapsed_time = time.time() - start_jitter_time
+		if verbose:
+			if jitter is not None:
+				print(f'Measured Jitter: {jitter:.2f} ms')
+			else:
+				print('Jitter measurement failed. Host may be unreachable.')
+			print(f'Time taken for jitter measurement: {jitter_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_retransmission_rate_flag:
+		start_rtxrate_time = time.time()
+		retransmission_rate = measure_retransmission_rate(host, port)
+		_measurements['retransmission_rate']['value'] = retransmission_rate
+		_measurements['retransmission_rate']['metric'] = '%'
+		rtxrate_elapsed_time = time.time() - start_rtxrate_time
+		if verbose:
+			print(f'Measured Retransmission Rate: {retransmission_rate:.2f}%')
+			print(f'Time taken for Retransmission rate measurement: {rtxrate_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_congestion_flag:
+		start_congestion_time = time.time()
+		congestion = calculate_congestion(duration/2)
+		_measurements['congestion']['value'] = congestion
+		_measurements['congestion']['metric'] = '%'
+		congestion_elapsed_time = time.time() - start_congestion_time
+		if verbose:
+			interface = 'Wi-Fi'  # FIXME: Check other possible interfaces
+			print(f"Network Utilization (%) on {interface}: {congestion[interface]:.2f}%")
+			print(f'Time taken for Network utilization measurement: {congestion_elapsed_time:.4f} seconds')
+			print('---------------------------')
+
+	if measure_interface_stats_flag:
+		interface_name = 'Wi-Fi'  # 'eth0'  # Replace with the desired network interface name
+		interface_stats = get_network_interface_statistics(interface_name)
+		_measurements['interface_stats']['value'] = interface_stats
+		if verbose:
+			if interface_stats:
+				print(f'Network Interface: {interface_name}')
+				print(f'Bytes Sent: {interface_stats.bytes_sent}')
+				print(f'Bytes Received: {interface_stats.bytes_recv}')
+				print(f'Packets Sent: {interface_stats.packets_sent}')
+				print(f'Packets Received: {interface_stats.packets_recv}')
+				print(f'Errors: {interface_stats.errin + interface_stats.errout}')
+				print(f'Collisions: {interface_stats.collisions}')
+			else:
+				print(f"Network interface '{interface_name}' not found.")
+			print('---------------------------')
+
+	total_elapsed_time = time.time() - start_time
+	if verbose:
+		print(f'Total time taken for all measurements: {total_elapsed_time:.4f} seconds')
+		print('---------------------------')
+
+	# Filter out entries with 'None' values in the 'value' key
+	_measurements = {key: value for key, value in _measurements.items() if value.get('value') is not None}
+
+	return _measurements, total_elapsed_time
+
+
+# Handle user interruption
+def signal_handler():
+	print('\nTraffic monitoring interrupted. Exiting...')
+	exit(0)
+
+
+# Main process
+if __name__ == '__main__':
+	# Data dictionary
+	data = {
+		'connection': {
+			'local': None,
+			'remote': None,
+			'port': None,
+		},
+		'measurements': None,
+		'timestamp': None,
+		'total_time_taken': None
+	}
+
+	# Register the signal handler for KeyboardInterrupt (Ctrl+C)
+	signal.signal(signal.SIGINT, signal_handler)
+
+	# CLI commands
+	parser = argparse.ArgumentParser(description='Network Measurement Script')
+	parser.add_argument("--verbose", action="store_true", help="Print verbose output")
+	parser.add_argument('--host', dest='remote_host', required=True, help='Specify the target host IP address')
+	parser.add_argument('--port', dest='target_port', type=int, default=80, help='Specify the target port (default: 80)')
+	parser.add_argument('--duration', dest='duration', type=int, default=4, help='Test duration (default: 4)')
+	parser.add_argument('--bandwidth', action='store_true', help='Enable bandwidth measurement')
+	parser.add_argument('--throughput', action='store_true', help='Enable throughput measurement')
+	parser.add_argument('--congestion', action='store_true', help='Enable network congestion measurement')
+	parser.add_argument('--packet-loss', action='store_true', help='Enable packet loss rate measurement')
+	parser.add_argument('--latency', action='store_true', help='Enable latency measurement')
+	parser.add_argument('--jitter', action='store_true', help='Enable jitter measurement')
+	parser.add_argument('--retransmission-rate', action='store_true', help='Enable retransmission rate measurement')
+	parser.add_argument('--interface-stats', action='store_true', help='Enable network interface statistics measurement')
+	args = parser.parse_args()
+
+	try:
+		measurements, elapsed_time = perform_measurements(
+			args.verbose,
+			args.remote_host,
+			args.target_port,
+			args.duration,
+			measure_bandwidth_flag=args.bandwidth,
+			measure_throughput_flag=args.throughput,
+			measure_congestion_flag=args.congestion,
+			measure_packet_loss_flag=args.packet_loss,
+			measure_latency_flag=args.latency,
+			measure_jitter_flag=args.jitter,
+			measure_retransmission_rate_flag=args.retransmission_rate,  # FIXME: Check it
+			measure_interface_stats_flag=args.interface_stats  # FIXME: Works better with congestion flag
+		)
+
+		# In case to retrieve information from origin host
+		# hostname = socket.gethostname()
+		# IPAddr = socket.gethostbyname(hostname)
+		# print("Your Computer Name is:" + hostname)
+		# print("Your Computer IP Address is:" + IPAddr)
+
+		# Extract local IP
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+		local_ip_address = s.getsockname()[0]
+
+		data['connection']['local'] = local_ip_address
+		data['connection']['remote'] = args.remote_host
+		data['connection']['port'] = args.target_port
+
+		# data['connection']['parameters']['protocol'] = args.protocol
+		# data['connection']['parameters']['duration'] = args.duration
+
+		data['measurements'] = measurements
+
+		data['timestamp'] = datetime.fromtimestamp(datetime.now().timestamp()).strftime("%d-%m-%Y, %H:%M:%S")
+		data['total_time_taken'] = round(elapsed_time, 2)
+
+
+		if args.verbose:
+			print(json.dumps(data, indent=4))
+
+	except PermissionError as e:
+		print('Error: Permission denied. Run the script with administrative privileges.')
+	except Exception as e:
+		print(f'Error: {e}')
